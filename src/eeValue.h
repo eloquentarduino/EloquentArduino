4444abca5deed3f91dea346cc635b3fbf53d20b0
SimoneS93 <salernosimone.uni@gmail.com>
2019-03-12 12:43:41 +0100

    [Submodule] eeValue

---
+++ src/eeValue.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <EEPROM.h>
+
+#define ELOQUENT_EEVALUE_CONTROL_BYTE 0x99
+
+
+namespace Eloquent {
+
+  /**
+   * Manipulate values stored in EEPROM as native types
+   */
+    template<class T>
+    class eeValue {
+    public:
+  
+        /**
+         * 
+         */
+        eeValue(uint16_t address) {
+            _value = 0;
+            _address = address;
+        }
+
+        /**
+         * 
+         */
+        eeValue& operator=(const T value) {
+            return set(value);
+        }
+  
+        /**
+         * 
+         */
+        T operator+(const T value) {
+            set(_value + value);
+
+            return value();
+        }
+  
+        /**
+         * 
+         */
+        T operator-(const T value) {
+            set(_value - value);
+
+            return value();
+        }
+  
+        /**
+         * 
+         */
+        T operator++() {
+            set(_value + 1);
+
+            return value();
+        }
+
+        /**
+         * 
+         */
+        T operator--() {
+            set(_value - 1);
+
+            return value();
+        }
+  
+        /**
+         * 
+         */
+        eeValue& operator+=(T value) {
+            return set(_value + value);
+        }
+
+        /**
+         * 
+         */
+        eeValue& operator-=(T value) {
+            return set(_value - value);
+        }
+  
+        /**
+         * Initialize value from EEPROM, if any
+         */
+        void begin(const T defaultValue) {
+            // check the control byte
+            // if present, load the stored value
+            if (EEPROM.read(_address) == ELOQUENT_EEVALUE_CONTROL_BYTE) {
+                readAnything(_value);
+            }
+            // if not present, initialize to default
+            else {
+                EEPROM.write(_address, ELOQUENT_EEVALUE_CONTROL_BYTE);
+                set(0);
+            }
+        }
+
+        /**
+         * Set value
+         */
+        eeValue& set(T value) {
+            if (value != _value) {
+                _value = value;
+                writeAnything(_value);
+            }
+
+            return *this;
+        }
+  
+        /**
+         * Get value
+         */
+        T value() {
+            return _value;
+        }
+  
+    protected:
+        uint16_t _address;
+        T _value;
+
+        /**
+         * @from https://playground.arduino.cc/Code/EEPROMWriteAnything
+         */
+        void readAnything(T& value) {
+            byte* p = (byte*)(void*)&value;
+
+            for (uint16_t i = 0; i < sizeof(value); i++)
+                *p++ = EEPROM.read(_address + 1 + i);
+        }
+
+        /**
+         * @from https://playground.arduino.cc/Code/EEPROMWriteAnything
+         */
+        void writeAnything(const T& value) {
+            const byte* p = (const uint8_t*)(const void*)&value;
+
+            for (uint16_t i = 0; i < sizeof(value); i++)
+                EEPROM.write(_address + 1 + i, *p++);
+        }
+    };
+}
