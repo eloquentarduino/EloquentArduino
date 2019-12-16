f7416ad1309a62cb1290426e5d244353835e537f
Agrimagsrl <web@agrimag.it>
2019-12-16 20:12:19 +0100

    fix examples/MicromlIrisExample

--- examples/MicromlIrisExample/MicromlIrisExample.ino
+++ examples/MicromlIrisExample/MicromlIrisExample.ino
@@ -1,24 +1,34 @@
 #include "iris.h"
 
+double features[4];
 
 void setup() {
     Serial.begin(115200);
 }
 
-
 void loop() {
     if (Serial.available()) {
-        double features[4];
-
         for (int i = 0; i < 4; i++) {
             String feature = Serial.readStringUntil(',');
 
             features[i] = atof(feature.c_str());
         }
 
+        printFeatures();
         Serial.print("Detected species: ");
         Serial.println(classIdxToName(predict(features)));
     }
 
     delay(10);
+}
+
+void printFeatures() {
+    const uint16_t numFeatures = sizeof(features) / sizeof(double);
+
+    Serial.print("You entered: ");
+
+    for (int i = 0; i < numFeatures; i++) {
+        Serial.print(features[i]);
+        Serial.print(i == numFeatures - 1 ? '\n' : ',');
+    }
 }
